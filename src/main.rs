ae78115838a773cebe1ba6d30a85066ad20281a5
Joost van der Laan <j.vanderlaan1@students.uu.nl>
2024-04-28 21:59:44 +0200

    UI includes hashing algorithm

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,19 @@ fn main() {
         .expect("Unexpected Error");
 }
 
+
+enum Message {
+    Hex(String),
+    Hash(String)
+}
+
 struct MultProgram {
-    input1:String,
-    output1:String,
-    input2:String,
-    output2:String,
+    input:String,
+    hex:String,
+    hash:String,
     thread_active:bool,
     tx:Sender<String>,
-    rx:Receiver<String>
+    rx:Receiver<Message>
 
 }
 
@@ -27,23 +32,25 @@ impl MultProgram {
     fn new(cc:&eframe::CreationContext<'_>) -> Self {
         cc.egui_ctx.set_visuals(egui::Visuals::dark());
         let (tx1, rx1) = mpsc::channel();
-        let (tx2, rx2) = mpsc::channel();
+        let (tx2, rx2) = mpsc::channel::<Message>();
         thread::spawn(move|| {
-            let tx:Sender<String> = tx2;
+            let tx:Sender<Message> = tx2;
             let rx:Receiver<String> = rx1;
             for s in rx.iter() {
                 let i = sha::parser::sha256_preprocessing(&s);
-                let res = sha::printer::print_blocks(&i,true);
-                tx.send(res).unwrap();
+                let hex_text = sha::printer::print_blocks(&i,true);
+                tx.send(Message::Hex(hex_text)).unwrap();
+                let hash = sha::sha256::sha_256(i);
+                let hash_text = sha::printer::print_blocks(&vec![hash],true);
+                tx.send(Message::Hash(hash_text)).unwrap();
             }
         });
         Self {
             tx: tx1, rx: rx2,
             thread_active:false,
-            input1: "".to_owned(),
-            input2: "".to_owned(),
-            output1: "".to_owned(),
-            output2: "".to_owned(),
+            input: "".to_owned(),
+            hex: "".to_owned(),
+            hash: "".to_owned(),
         }
     }
 }
@@ -53,8 +60,9 @@ impl eframe::App for MultProgram {
         egui::CentralPanel::default().show(ctx, |ui| {
             if self.thread_active {
                 match self.rx.try_recv() {
-                    Ok(s) => {
-                        self.output2 = s;
+                    Ok(Message::Hex(text)) => self.hex = text,
+                    Ok(Message::Hash(text)) => {
+                        self.hash = text;
                         self.thread_active = false;
                     },
                     Err(_) => (), 
@@ -64,12 +72,23 @@ impl eframe::App for MultProgram {
             ui.group(|ui| {
                 egui::Grid::new("stuff").show(ui, |ui| {
                     ui.label("[IN]:");
-                    ui.add_sized((520., 20.), egui::TextEdit::singleline(&mut self.input2).hint_text("Input Text..."));
+                    ui.add_sized((520., 20.), egui::TextEdit::singleline(&mut self.input).hint_text("Input Text..."));
                     if ui.button("Submit").clicked() && !self.thread_active {
-                        self.tx.send(self.input2.trim().to_owned()).unwrap();
+                        self.tx.send(self.input.trim().to_owned()).unwrap();
                         self.thread_active = true;
-                        self.output2 = "[Loading...]".to_owned();
+                        self.hex = "[Loading...]".to_owned();
+                        self.hash = "[Loading...]".to_owned();
                     }
+                    ui.end_row();
+                    ui.end_row();
+                    ui.label("[HEX]:"); 
+                    ui.with_layout(egui::Layout::left_to_right(egui::Align::Center), |ui| {
+
+                        //ui.style_mut().visuals.override_text_color = Some(egui::Color32::WHITE);
+                        ui.style_mut().override_font_id = Some(FontId::monospace(12.));
+                        ui.add(egui::Label::new(&self.hex).selectable(true).wrap(true));
+                    });
+
                     ui.end_row();
                     ui.end_row();
                     ui.label("[OUT]:"); 
@@ -77,8 +96,9 @@ impl eframe::App for MultProgram {
 
                         //ui.style_mut().visuals.override_text_color = Some(egui::Color32::WHITE);
                         ui.style_mut().override_font_id = Some(FontId::monospace(12.));
-                        ui.add(egui::Label::new(&self.output2).selectable(true).wrap(true));
+                        ui.add(egui::Label::new(&self.hash).selectable(true).wrap(true));
                     })
+                    
                     
                 });
                 
